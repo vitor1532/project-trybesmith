20d1cf754afdec16f97e749477060d3aa1a76854
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-23 22:11:36 -0300

    feat: case of success on product.service.update

---
+++ tests/mocks/products.service.mocks.ts
@@ -0,0 +1,24 @@
+export const invalidProduct = {
+  id: 999,
+  name: 'bandeija de bronze',
+  price: '80 peças de prata',
+};
+
+export const validProduct = {
+  name: 'Martelo do thor',
+  price: '99999 peças de prata',
+};
+
+export const validProductFromModel = {
+  id: 1,
+  name: 'Martelo do thor',
+  price: '80 peças de prata',
+  userId: 2
+};
+
+export const updatedProduct = {
+  id: 1,
+  name: 'Martelo do thor',
+  price: '99999 peças de prata',
+  userId: 2
+}
--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -2,27 +2,48 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import ProductModel from '../../../src/database/models/product.model';
 import ProductService from '../../../src/services/products.service';
+import { 
+  invalidProduct,
+  updatedProduct,
+  validProduct,
+  validProductFromModel,
+} from '../../mocks/products.service.mocks';
 
 
 describe('ProductsService', function () {
   beforeEach(function () { sinon.restore(); });
   const NOT_FOUND = 'NOT_FOUND';
+  const SUCCESS = 'SUCCESS';
 
   it('Test the update function in case of a invalid id', async function() {
     //arrange
-    const falseProduct = {
-      id: 999,
-      name: 'bandeija de bronze',
-      price: '80 peças de prata',
-      userId: 1
-    };
-    sinon.stub(ProductModel, 'findByPk').resolves(null);
+
+    sinon.stub(ProductModel, 'findOne').resolves(null);
 
     //act
-    const serviceResponse = await ProductService.update(falseProduct);
+    const serviceResponse = await ProductService.update(invalidProduct);
 
     //assert
     expect(serviceResponse.status).to.be.eq(NOT_FOUND);
     expect(serviceResponse.data).to.deep.equal({message: 'Product not found'});
-  })
+  });
+
+  it.only('Test the update function in case of success', async function() {
+    //arrange
+    const productMock = ProductModel.build(validProductFromModel);
+    const updatedMock = ProductModel.build(updatedProduct);
+    const productSinon = sinon.stub(ProductModel, 'findOne');
+    productSinon.onCall(0).resolves(productMock);
+    productSinon.onCall(1).resolves(updatedMock);
+    sinon.stub(ProductModel, 'update').resolves([1]);
+
+    //act
+    const serviceResponse = await ProductService.update(validProduct);
+
+    Promise.all([serviceResponse]);
+
+    //assert
+    expect(serviceResponse.status).to.be.eq(SUCCESS);
+    expect(serviceResponse.data).to.deep.equal(updatedProduct);
+  });
 });
