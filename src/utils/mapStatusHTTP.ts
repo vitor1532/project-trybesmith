9bcfd680ef538a4ea5d00b520b173bd58bb50775
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-23 22:29:55 -0300

    feat: adding status responses on the object

--- src/utils/mapStatusHTTP.ts
+++ src/utils/mapStatusHTTP.ts
@@ -1,9 +1,11 @@
 export default function mapStatusHTTP(status: string): number {
   const statusHTTPMap: Record<string, number> = {
     SUCCESS: 200,
+    CREATED: 201,
     INVALID_DATA: 400,
     UNAUTHORIZED: 401,
     NOT_FOUND: 404,
+    UNPROCESSABLE: 422,
   };
 
   return statusHTTPMap[status] ?? 500;
