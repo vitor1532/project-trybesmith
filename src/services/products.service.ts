267ed05f37ba40ebc1af1ada087c257cc05ff757
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-23 21:08:31 -0300

    feat: case of invalid user validation

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,10 +1,13 @@
 import ProductModel from '../database/models/product.model';
 import { Product } from '../types/Product';
 import { ServiceResponse } from '../types/ServiceResponse';
+import validations from './utils/validations';
 
 const update = async ({ id, name, price, userId }: Product): Promise<ServiceResponse> => {
   const productFound = await ProductModel.findByPk(id);
   if (!productFound) return { status: 'NOT_FOUND', data: { message: 'Product not found' } };
+
+  if (productFound.dataValues.userId !== userId) await validations.isUserValid(userId);
 
   await ProductModel.update({ name, price, userId }, { where: { id } });
 
