2539c67df8508cfd60d3817322325414f3b1f8ef
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-23 21:32:41 -0300

    fix: spelling error

--- src/services/utils/validations.ts
+++ src/services/utils/validations.ts
@@ -4,7 +4,7 @@ import { ServiceResponse } from '../../types/ServiceResponse';
 const isUserValid = async (userId: number): Promise<ServiceResponse | undefined> => {
   const userFound = await UserModel.findByPk(userId);
 
-  if (!userFound) return { status: 'NOT_FOUND', data: { message: 'user not found!' } };
+  if (!userFound) return { status: 'NOT_FOUND', data: { message: 'User not found!' } };
 };
 
 export default {
