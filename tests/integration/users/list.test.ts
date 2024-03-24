128ada4c43209efa59877900b1e2cff1c5ac0914
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-24 13:48:24 -0300

    feat: integration test for endpoint /users

--- tests/integration/users/list.test.ts
+++ tests/integration/users/list.test.ts
@@ -1,10 +1,35 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
-
+import UserModel from '../../../src/database/models/user.model';
+import { 
+  usersFromService,
+  validUserFromModel,
+  secondValidUserFromModel,
+  getAllUsersSuccessfulResponse
+} from '../../mocks/user.service.mocks';
+import app from '../../../src/app';
+import usersService from '../../../src/services/users.service';
 chai.use(chaiHttp);
 
 describe('GET /users', function () { 
   beforeEach(function () { sinon.restore(); });
-
+  it.only('Tests GET /products in case of success', async function () {
+    // arrange
+    const usersMock = [
+      UserModel.build(validUserFromModel),
+      UserModel.build(secondValidUserFromModel)
+    ];
+    
+    sinon.stub(UserModel, 'findAll').resolves(usersMock);
+    sinon.stub(usersService, 'getAll').resolves(getAllUsersSuccessfulResponse);
+    // act
+    const httpResponse = await chai.request(app)
+      .get('/users')
+      .send();
+  
+    //assert
+    expect(httpResponse.status).to.equal(200);
+    expect(httpResponse.body).to.deep.equal(usersFromService);
+  });
 });
--- tests/mocks/user.service.mocks.ts
+++ tests/mocks/user.service.mocks.ts
@@ -1,9 +1,44 @@
+import { User } from "../../src/types/User";
+
 export const validUserFromModel = {
   id: 1,
   username: 'Hagar',
   vocation: 'Guerreiro',
   level: 10,
   password: '$3cr3t'
+}
+
+export const secondValidUserFromModel = {
+  id: 2,
+  username: 'Thor',
+  vocation: 'Guerreiro',
+  level: 99,
+  password: '$3cr3t'
+}
+
+export const validUserFromService = {
+  username: 'Hagar',
+  productIds: [1, 2]
+}
+
+export const secondValidUserFromService = {
+  username: 'Thor',
+  productIds: [2]
+}
+
+export const usersArray = [
+  validUserFromModel,
+  secondValidUserFromModel
+];
+
+export const usersFromService = [
+  validUserFromService,
+  secondValidUserFromService
+];
+
+export const getAllUsersSuccessfulResponse = {
+  status: 'SUCCESS',
+  data: usersFromService as User[]
 }
 
 export const userNotFoundServiceResponse = {
