b1e84c854799a95d2f46c07e01750af491656f1a
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-24 14:43:14 -0300

    feat: verifyLoginFields.middleware

---
+++ src/middlewares/verifyLoginFields.middleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+
+const verifyLoginFields = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: '"username" and "password" are required' });
+  }
+  next();
+};
+
+export default verifyLoginFields;
