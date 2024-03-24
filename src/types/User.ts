1209557da7b75817d2bbba4cea57dd7b7ada24fa
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-24 13:33:24 -0300

    fix: adapting User type

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,8 +1,9 @@
+export type ProductIds = { id: number } | number;
 export type User = {
   id: number;
   username: string;
   vocation: string;
   level: number;
   password: string;
-  productIds?: number[];
+  productIds?: ProductIds[];
 };
