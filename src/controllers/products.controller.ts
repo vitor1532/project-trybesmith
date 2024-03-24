a52098de598e6b454f048250cd1591c807116801
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-23 22:47:21 -0300

    feat: creating products.controller.create

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -2,6 +2,14 @@ import { Request, Response } from 'express';
 import ProductsService from '../services/products.service';
 import mapStatusHTTP from '../utils/mapStatusHTTP';
 
+// TODO: VERIFY REQ.BODY 
+
+const create = async (req: Request, res: Response): Promise<Response> => {
+  const { status, data } = await ProductsService.create(req.body);
+
+  return res.status(mapStatusHTTP(status)).json(data);
+};
+
 // TODO: VERIFY REQ.BODY (see 'extraindo contrato a partir dos types')
 
 const update = async (req: Request, res: Response): Promise<Response> => {
@@ -12,4 +20,5 @@ const update = async (req: Request, res: Response): Promise<Response> => {
 
 export default {
   update,
+  create,
 };
