df07c664d895b902000bbfe36b2394c555ae786c
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-24 00:13:47 -0300

    fix: genericts from user.service.getById

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,7 +1,8 @@
 import UserModel from '../database/models/user.model';
 import { ServiceResponse } from '../types/ServiceResponse';
+import { User } from '../types/User';
 
-const findById = async (id: number): Promise<ServiceResponse> => {
+const findById = async (id: number): Promise<ServiceResponse<User>> => {
   const userFound = await UserModel.findByPk(id);
 
   if (!userFound) return { status: 'NOT_FOUND', data: { message: 'user not found!' } };
