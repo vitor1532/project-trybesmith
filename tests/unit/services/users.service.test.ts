c7fff498199e602ef32aba24fbcd5d448d192fa0
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-24 14:17:07 -0300

    unit tests for user.service.findByID

--- tests/unit/services/users.service.test.ts
+++ tests/unit/services/users.service.test.ts
@@ -1,15 +1,16 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import UserModel from '../../../src/database/models/user.model';
-import { secondValidUserFromModel, validUserFromModel, validUsersFromModel } from '../../mocks/user.service.mocks';
+import { secondValidUserFromModel, userNotFoundServiceResponse, validUserFromModel, validUsersFromModel } from '../../mocks/user.service.mocks';
 import UsersService from '../../../src/services/users.service';
 import ProductModel from '../../../src/database/models/product.model';
 import { secondvalidProductFromModel, validProductFromModel } from '../../mocks/products.mocks';
 
 describe('UsersService', function () {
   const SUCCESS = 'SUCCESS';
+  const NOT_FOUND = 'NOT_FOUND';
   beforeEach(function () { sinon.restore(); });
-  it.only('Test the getAll function in case of success', async function() {
+  it('Test the getAll function in case of success', async function() {
     //arrange
     const usersMock = [
       UserModel.build(validUserFromModel),
@@ -38,5 +39,32 @@ describe('UsersService', function () {
         attributes: ['id'],
       }],
     });
+  });
+
+  it('Test the findById function in case of success', async function() {
+    //arrange
+    const id = 1;
+    const userMock = UserModel.build(validUserFromModel);
+    sinon.stub(UserModel, 'findByPk').resolves(userMock);
+
+    //act
+    const serviceResponse = await UsersService.findById(id);
+
+    //assert
+    expect(serviceResponse.status).to.be.eq(SUCCESS);
+    expect(serviceResponse.data).to.deep.equal(validUserFromModel);
+  });
+
+  it('Test the findById function in case of invalid Id', async function() {
+    //arrange
+    const id = 999;
+    sinon.stub(UserModel, 'findByPk').resolves(null);
+
+    //act
+    const serviceResponse = await UsersService.findById(id);
+
+    //assert
+    expect(serviceResponse.status).to.be.eq(NOT_FOUND);
+    expect(serviceResponse.data).to.deep.equal({ message: 'user not found!' });
   });
 });
