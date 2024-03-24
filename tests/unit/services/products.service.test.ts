3159bcd15c1fc587bc9e60e95e24e95418275385
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-23 21:09:30 -0300

    feat: tests for invalid product id on products.service.update

--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -1,7 +1,28 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import ProductModel from '../../../src/database/models/product.model';
+import ProductService from '../../../src/services/products.service';
+
 
 describe('ProductsService', function () {
   beforeEach(function () { sinon.restore(); });
+  const NOT_FOUND = 'NOT_FOUND';
 
+  it('Test the update function in case of a invalid id', async function() {
+    //arrange
+    const falseProduct = {
+      id: 999,
+      name: 'bandeija de bronze',
+      price: '80 peças de prata',
+      userId: 1
+    };
+    sinon.stub(ProductModel, 'findByPk').resolves(null);
+
+    //act
+    const serviceResponse = await ProductService.update(falseProduct);
+
+    //assert
+    expect(serviceResponse.status).to.be.eq(NOT_FOUND);
+    expect(serviceResponse.data).to.deep.equal({message: 'Product not found'});
+  })
 });
